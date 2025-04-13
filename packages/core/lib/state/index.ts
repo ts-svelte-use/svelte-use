9c26480c994ed842f886a7b6718f643b4c0e9463
MohamedElmdary <engm5081@gmail.com>
2025-04-13 03:49:55 +0200

    🔥 feat: Add useLastChanged function to core lib

--- packages/core/lib/state/index.ts
+++ packages/core/lib/state/index.ts
@@ -1,3 +1,4 @@
 export { createGlobalState } from "./createGlobalState/index.js"
 export { createInjectionState } from "./createInjectionState/index.js"
 export { useAsyncState } from "./useAsyncState/index.svelte.js"
+export { useLastChanged } from "./useLastChanged/index.svelte.js"
